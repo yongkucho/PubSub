2c133a98fdf560ef7ce646befe2104f2dddadabd
yongkucho <yongkucho@users.noreply.github.com>
2014-05-26 17:20:31 +0900

    Create PubSub.js

---
+++ PubSub.js
@@ -0,0 +1,17 @@
+/*
+PubSub Module
+
+PubSub.pub("name","yongkucho");
+PubSub.sub("name");
+*/
+var PubSub = (function(){
+    var store = {};
+    return {
+        pub: function(topic, msg){
+            store[topic] = msg;
+        },
+        sub: function(topic){
+            return store[topic];
+        }
+    };
+}());
